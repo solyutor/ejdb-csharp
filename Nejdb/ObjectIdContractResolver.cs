98a4c4e590432535dc8e73478f64341e8d9584be
Jury Soldatenkov <yurysold@gmail.com>
2014-02-27 13:13:24 +0400

    Always serialize object id as first field (fix #19)

--- Nejdb/ObjectIdContractResolver.cs
+++ Nejdb/ObjectIdContractResolver.cs
@@ -10,24 +10,47 @@ namespace Nejdb
 {
     internal class ObjectIdContractResolver : DefaultContractResolver
     {
+        public const string IdPropertyName = "_id";
         public static readonly ObjectIdContractResolver Instance = new ObjectIdContractResolver();
 
         protected override IList<JsonProperty> CreateProperties(Type type, MemberSerialization memberSerialization)
         {
-            var jsonProperties = base.CreateProperties(type, memberSerialization);
+            IList<JsonProperty> orignalProperties = base.CreateProperties(type, memberSerialization);
+
+            List<JsonProperty> jsonProperties = new List<JsonProperty>(orignalProperties.Count);
+            jsonProperties.AddRange(orignalProperties);
+
             var idProperty = jsonProperties.SingleOrDefault(
                 x => x.PropertyType == typeof(ObjectId) &&
                      (x.PropertyName.Equals("Id", StringComparison.OrdinalIgnoreCase) ||
                       (x.PropertyName.Equals("_Id", StringComparison.OrdinalIgnoreCase))));
 
+
             if (idProperty != null)
             {
                 var originalName = idProperty.PropertyName;
-                idProperty.PropertyName = "_id";
+                idProperty.PropertyName = IdPropertyName;
                 idProperty.ShouldSerialize = value => !TypeExtension.IdIsEmpty(value, originalName);
+
+                jsonProperties.Sort(IdMustBeFirst);
             }
 
             return jsonProperties;
+        }
+
+        private int IdMustBeFirst(JsonProperty x, JsonProperty y)
+        {
+            if (x.PropertyName == IdPropertyName)
+            {
+                return int.MinValue;
+            }
+
+            if (y.PropertyName == IdPropertyName)
+            {
+                return int.MaxValue;
+            }
+
+            return Comparer<string>.Default.Compare(x.PropertyName, y.PropertyName);
         }
     }
 }
