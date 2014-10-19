d82c6b8c20128086a4194a103c3299b2623e02e6
Jury Soldatenkov <yurysold@googlemail.com>
2014-10-19 22:46:19 +0400

    Written few xml-comments

--- Nejdb/Queries/AndCriterion.cs
+++ Nejdb/Queries/AndCriterion.cs
@@ -2,15 +2,24 @@ using Newtonsoft.Json;
 
 namespace Nejdb.Queries
 {
+    /// <summary>
+    /// Represents logical AND between any number of inner criterions
+    /// </summary>
     public class AndCriterion : ICriterion
     {
         private readonly ICriterion[] _criterions;
 
+        /// <summary>
+        /// Creates new instance of <see cref="AndCriterion"/>
+        /// </summary>
         public AndCriterion(ICriterion[] criterions)
         {
             _criterions = criterions;
         }
 
+        /// <summary>
+        /// Writes criterion to specified writer.
+        /// </summary>
         public void WriteTo(JsonWriter writer)
         {
             //Example: {z : 33, $and : [ {$or : [{a : 1}, {b : 2}]}, {$or : [{c : 5}, {d : 7}]} ] }
--- Nejdb/Queries/OrCriterion.cs
+++ Nejdb/Queries/OrCriterion.cs
@@ -1,17 +1,25 @@
-using System;
 using Newtonsoft.Json;
 
 namespace Nejdb.Queries
 {
+    /// <summary>
+    /// Represents logical OR operation between any number of inner criterions
+    /// </summary>
     public class OrCriterion : ICriterion
     {
         private readonly ICriterion[] _criterions;
 
+        /// <summary>
+        /// Creates new instance of <see cref="AndCriterion"/>
+        /// </summary>
         public OrCriterion(ICriterion[] criterions)
         {
             _criterions = criterions;
         }
 
+        /// <summary>
+        /// Writes criterion to specified writer.
+        /// </summary>
         public void WriteTo(JsonWriter writer)
         {
             //Example: {z : 33, $and : [ {$or : [{a : 1}, {b : 2}]}, {$or : [{c : 5}, {d : 7}]} ] }
